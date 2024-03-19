d708f45d815ceb15288fc9df1a969104bec96286
kwonymo88 <39462665+kwonymo88@users.noreply.github.com>
2024-03-19 11:44:58 +0900

    Add EnhancedInputSystem On Aura

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,24 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* SubInputSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(SubInputSystem);
+	SubInputSystem->AddMappingContext(AuraContext, 0);
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetHideCursorDuringCapture(false);
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	SetInputMode(InputModeData);
+}
